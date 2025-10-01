c72e7ee6ad51c81733457bb424029cc83fcd7d76
Saran440 <saranl@ecosoft.co.th>
2025-10-01 13:57:27 +0700

    [IMP] l10n_th_partner: use company_registry instead new branch
    (cherry picked from commit 1293cefe593e0f12f238705e108e6307305afff9)

--- l10n_th_partner/tests/test_l10n_th_partner.py
+++ l10n_th_partner/tests/test_l10n_th_partner.py
@@ -74,14 +74,14 @@ class TestL10nThPartner(TransactionCase):
                 "firstname": "Firstname",
                 "lastname": "Lastname",
                 "vat": "0123456789012",
-                "branch": "00000",
+                "company_registry": "00000",
                 "company_id": self.main_company.id,
             }
         )
         partner2 = partner1.copy()
         with self.assertRaises(ValidationError):
             with Form(partner2) as p2:
-                p2.branch = "00000"
+                p2.company_registry = "00000"
 
     def test_duplicate_partner_vat_branch_no_company(self):
         """Test VAT/branch constraint without company_id"""
@@ -90,11 +90,11 @@ class TestL10nThPartner(TransactionCase):
                 "firstname": "First",
                 "lastname": "Last",
                 "vat": "9999999999999",
-                "branch": "00001",
+                "company_registry": "00001",
                 "company_id": False,
             }
         )
         partner2 = partner1.copy()
         with self.assertRaises(ValidationError):
             with Form(partner2) as p2:
-                p2.branch = "00001"
+                p2.company_registry = "00001"
